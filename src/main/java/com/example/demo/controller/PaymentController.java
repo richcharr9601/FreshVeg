61f98782c0c749543b2b8455d470132c928a7443
Tran Toan <tofbest267@gmail.com>
2023-04-21 00:53:25 +0700

    feat: get order info

--- src/main/java/com/example/demo/controller/PaymentController.java
+++ src/main/java/com/example/demo/controller/PaymentController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URLEncoder;
@@ -24,6 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.demo.config.PaymentConfig;
 import com.example.demo.dto.TransactionStatusDTO;
@@ -38,7 +38,6 @@ import com.example.demo.service.imp.UserService;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 
-
 @RequiredArgsConstructor
 @RestController
 public class PaymentController {
@@ -49,134 +48,131 @@ public class PaymentController {
     private final UserRepository userRepository;
     private final OrderRepository orderRepository;
 
-  
-
-
     @PostMapping("/checkout/create-payment")
-    public ResponseEntity<?> createPayment(@RequestBody Payment requestParams)  {
-                    // String TXNREF = PaymentConfig.getRandomNumber(5);
+    public ResponseEntity<?> createPayment(@RequestBody Payment requestParams) {
+        // String TXNREF = PaymentConfig.getRandomNumber(5);
         Order order = orderRepository.findByOrderId(requestParams.getOrderId());
-        
-                    int amount = order.getAmount() * 100;
-                    Map<String, String> vnp_Params = new HashMap<>();
-                    vnp_Params.put("vnp_Version", PaymentConfig.VERSIONVNPAY);
-                    vnp_Params.put("vnp_Command", PaymentConfig.COMMAND);
-                    vnp_Params.put("vnp_TmnCode", PaymentConfig.TMNCODE);
-                    vnp_Params.put("vnp_Amount", String.valueOf(amount));
-                    vnp_Params.put("vnp_CurrCode", PaymentConfig.CURRCODE);
-                    // if (requestParams.getBankCode() != null && !requestParams.getBankCode().isEmpty()) {
-                    vnp_Params.put("vnp_BankCode", requestParams.getBankCode());
-                    vnp_Params.put("vnp_Locale", PaymentConfig.LOCALEDEFAULT);
-                    // vnp_Params.put("vnp_CardType", PaymentConfig.CARDTYPE);
-                    vnp_Params.put("vnp_TxnRef", String.valueOf(order.getOrderId()));
-                    vnp_Params.put("vnp_OrderInfo", "Thanh toan hoa don" + order.getOrderId());
-                    vnp_Params.put("vnp_OrderType", PaymentConfig.ORDERTYPE);
-                    vnp_Params.put("vnp_ReturnUrl", PaymentConfig.RETURNURL);
-                    vnp_Params.put("vnp_IpAddr", PaymentConfig.IPDEFAULT);
 
-    Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
-    SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
-    String vnp_CreateDate = formatter.format(cld.getTime());
-    vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
-    
-    cld.add(Calendar.MINUTE, 15);
-    String vnp_ExpireDate = formatter.format(cld.getTime());
-    vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
-    
-    List fieldNames = new ArrayList(vnp_Params.keySet());
-    Collections.sort(fieldNames);
-    StringBuilder hashData = new StringBuilder();
-    StringBuilder query = new StringBuilder();
-    Iterator itr = fieldNames.iterator();
-    while (itr.hasNext()) {
-        String fieldName = (String) itr.next();
-        String fieldValue = (String) vnp_Params.get(fieldName);
-        if ((fieldValue != null) && (fieldValue.length() > 0)) {
-            //Build hash data
-            hashData.append(fieldName);
-            hashData.append('=');
-            try {
-                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
-            } catch (UnsupportedEncodingException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            //Build query
-            try {
-                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
-            } catch (UnsupportedEncodingException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            query.append('=');
-            try {
-                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
-            } catch (UnsupportedEncodingException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            if (itr.hasNext()) {
-                query.append('&');
-                hashData.append('&');
+        int amount = order.getAmount() * 100;
+        Map<String, String> vnp_Params = new HashMap<>();
+        vnp_Params.put("vnp_Version", PaymentConfig.VERSIONVNPAY);
+        vnp_Params.put("vnp_Command", PaymentConfig.COMMAND);
+        vnp_Params.put("vnp_TmnCode", PaymentConfig.TMNCODE);
+        vnp_Params.put("vnp_Amount", String.valueOf(amount));
+        vnp_Params.put("vnp_CurrCode", PaymentConfig.CURRCODE);
+        // if (requestParams.getBankCode() != null &&
+        // !requestParams.getBankCode().isEmpty()) {
+        vnp_Params.put("vnp_BankCode", requestParams.getBankCode());
+        vnp_Params.put("vnp_Locale", PaymentConfig.LOCALEDEFAULT);
+        // vnp_Params.put("vnp_CardType", PaymentConfig.CARDTYPE);
+        vnp_Params.put("vnp_TxnRef", String.valueOf(order.getOrderId()));
+        vnp_Params.put("vnp_OrderInfo", "Thanh toan hoa don" + order.getOrderId());
+        vnp_Params.put("vnp_OrderType", PaymentConfig.ORDERTYPE);
+        vnp_Params.put("vnp_ReturnUrl", PaymentConfig.RETURNURL);
+        vnp_Params.put("vnp_IpAddr", PaymentConfig.IPDEFAULT);
+
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        String vnp_CreateDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+
+        cld.add(Calendar.MINUTE, 15);
+        String vnp_ExpireDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+
+        List fieldNames = new ArrayList(vnp_Params.keySet());
+        Collections.sort(fieldNames);
+        StringBuilder hashData = new StringBuilder();
+        StringBuilder query = new StringBuilder();
+        Iterator itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = (String) itr.next();
+            String fieldValue = (String) vnp_Params.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                // Build hash data
+                hashData.append(fieldName);
+                hashData.append('=');
+                try {
+                    hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                } catch (UnsupportedEncodingException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                // Build query
+                try {
+                    query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                } catch (UnsupportedEncodingException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                query.append('=');
+                try {
+                    query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                } catch (UnsupportedEncodingException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                if (itr.hasNext()) {
+                    query.append('&');
+                    hashData.append('&');
+                }
             }
         }
-    }
-    String queryUrl = query.toString();
-    String vnp_SecureHash = PaymentConfig.hmacSHA512(PaymentConfig.CHECKSUM, hashData.toString());
-    queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
-    String paymentUrl = PaymentConfig.VNPAYURL + "?" + queryUrl;
-    PaymentRes result = new PaymentRes();
-    result.setStatus("00");
-    result.setMessage("Success");
-    result.setUrl(paymentUrl);
-    return ResponseEntity.status(HttpStatus.OK).body(result); 
-}
-
-
-
-
-
-
-@GetMapping("/checkout/payment-information")
-public ResponseEntity<?>  transactionHandle (
-    @RequestParam(value = "vnp_Amount", required = false) String amount,
-    @RequestParam(value = "vnp_BankCode", required = false) String bankCode,
-    @RequestParam(value = "vnp_BankTranNo", required = false) String bankTranNo,
-    @RequestParam(value = "vnp_CardType", required = false) String cardType,
-    @RequestParam(value = "vnp_PayDate", required = false) String payDate,
-    @RequestParam(value = "vnp_OrderInfo", required = false) String orderInfo,
-    @RequestParam(value = "vnp_ResponseCode", required = false) String responseCode,
-    @RequestParam(value = "vnp_TransactionNo", required = false) String transactionNo,
-    @RequestParam(value = "vnp_TxnRef", required = false) String txnRef,
-    @RequestParam(value = "vnp_SecureHash", required = false) String secureHash,
-    @RequestParam(value = "vnp_SecureHashType", required = false) String secureHashType
-) throws MessagingException{
-    TransactionStatusDTO result = new TransactionStatusDTO();
-    if (!responseCode.equalsIgnoreCase("00")){
-        result.setStatus("02");
-        result.setMessage("Checkout failed");
-        return ResponseEntity.status(HttpStatus.OK).body(result);
-    }
-
-    Order order = orderRepository.findByOrderId(Long.parseLong(txnRef));
- 
-    if(order==null){
-        result.setStatus("01");
-        result.setMessage("Cannot find order");
-        return ResponseEntity.status(HttpStatus.OK).body(result);
-    }
-    if(order.getStatusPayment()==true){
-        result.setStatus("01");
-        result.setMessage("Order already paid");
-        return ResponseEntity.status(HttpStatus.OK).body(result);
-    }
-    if(order.getOrderId().toString().equalsIgnoreCase(txnRef)){
-        order.setStatusPayment(true);
-        orderRepository.save(order);
+        String queryUrl = query.toString();
+        String vnp_SecureHash = PaymentConfig.hmacSHA512(PaymentConfig.CHECKSUM, hashData.toString());
+        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+        String paymentUrl = PaymentConfig.VNPAYURL + "?" + queryUrl;
+        PaymentRes result = new PaymentRes();
         result.setStatus("00");
-        result.setMessage("Checkout successfully");
-        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create("http://localhost:5173/order-success")).body(result);
+        result.setMessage("Success");
+        result.setUrl(paymentUrl);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
-    return ResponseEntity.status(HttpStatus.OK).body(result);
-}
+
+    @GetMapping("/checkout/payment-information")
+    public ResponseEntity<?> transactionHandle(
+            @RequestParam(value = "vnp_Amount", required = false) String amount,
+            @RequestParam(value = "vnp_BankCode", required = false) String bankCode,
+            @RequestParam(value = "vnp_BankTranNo", required = false) String bankTranNo,
+            @RequestParam(value = "vnp_CardType", required = false) String cardType,
+            @RequestParam(value = "vnp_PayDate", required = false) String payDate,
+            @RequestParam(value = "vnp_OrderInfo", required = false) String orderInfo,
+            @RequestParam(value = "vnp_ResponseCode", required = false) String responseCode,
+            @RequestParam(value = "vnp_TransactionNo", required = false) String transactionNo,
+            @RequestParam(value = "vnp_TxnRef", required = false) String txnRef,
+            @RequestParam(value = "vnp_SecureHash", required = false) String secureHash,
+            @RequestParam(value = "vnp_SecureHashType", required = false) String secureHashType)
+            throws MessagingException {
+        TransactionStatusDTO result = new TransactionStatusDTO();
+        if (!responseCode.equalsIgnoreCase("00")) {
+            result.setStatus("02");
+            result.setMessage("Checkout failed");
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+        }
+
+        Order order = orderRepository.findByOrderId(Long.parseLong(txnRef));
+
+        if (order == null) {
+            result.setStatus("01");
+            result.setMessage("Cannot find order");
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+        }
+        if (order.getStatusPayment() == true) {
+            result.setStatus("01");
+            result.setMessage("Order already paid");
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+        }
+        if (order.getOrderId().toString().equalsIgnoreCase(txnRef)) {
+            order.setStatusPayment(true);
+            orderRepository.save(order);
+            result.setStatus("00");
+            result.setMessage("Checkout successfully");
+
+            var uri = UriComponentsBuilder.fromHttpUrl("http://localhost:5173/order-success/")
+                    .queryParam("orderId", txnRef)
+                    .build();
+            return ResponseEntity.status(HttpStatus.FOUND).location(uri.toUri()).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
 }
