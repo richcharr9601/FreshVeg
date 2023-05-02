cc02a0e1f5b4e9ba62c1f067b203b733a1109c47
Leanhduc9601 <91675657+Leanhduc9601@users.noreply.github.com>
2023-05-02 15:39:48 +0700

    fix top10

--- src/main/java/com/example/demo/dto/ProductStatisticDTO.java
+++ src/main/java/com/example/demo/dto/ProductStatisticDTO.java
@@ -2,7 +2,7 @@ package com.example.demo.dto;
 
 
 public interface ProductStatisticDTO { 
-    String getProductName();
-    String getProductWeight();
+    String getProduct_name();
+    String getWeight();
     Long getOrderCount();
 }
